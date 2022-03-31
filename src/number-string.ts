f8d485d94be0ed8d5fc69a49df3a8ea1df8cff19
jin1125 <shogo.jinta@gmail.com>
2022-03-31 22:16:46 +0900

    16

---
+++ src/number-string.ts
@@ -0,0 +1,11 @@
+export {};
+
+let year: number = 1992;
+// year = false;
+console.log(year);
+
+let age: number = 0x2b;
+console.log(age);
+
+let name: string = 'jin'
+console.log(name);
---
+++ src/taple.ts
@@ -0,0 +1,4 @@
+export {};
+
+let profile: [string, number] = ['jin', 29]
+// profile = [29, 'jin']
