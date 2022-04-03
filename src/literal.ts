190cb0bcd678d98c94b80d4a872e8d8426fa20fe
jin1125 <shogo.jinta@gmail.com>
2022-04-03 18:02:55 +0900

    28

---
+++ src/literal.ts
@@ -0,0 +1,12 @@
+export {}
+
+let dayWeek: '日' | '月' | '火' | '水' | '木' | '金' | '土';
+dayWeek = '月'
+// dayWeek = '31'
+
+let month: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 = 1
+month = 10
+// month = 11
+
+let TRUE: true = true
+// TRUE = false
