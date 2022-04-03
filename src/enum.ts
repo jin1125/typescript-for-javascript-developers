4970eaced4a1de521e2699ceb9c4a080b39e750b
jin1125 <shogo.jinta@gmail.com>
2022-04-03 19:01:25 +0900

    29

---
+++ src/enum.ts
@@ -0,0 +1,36 @@
+export {}
+
+enum Months {
+  January = 1,
+  Febuary,
+  March,
+  April,
+  May,
+  Jun,
+  July,
+  August,
+  September,
+  October,
+  November,
+  December
+}
+
+Months.Febuary
+
+enum COLORS {
+  RED = '#FF0000',
+  WHITE = '#FFFFFF',
+  GREEN = '#008000',
+  BLUE = '#0000FF',
+  BLACK = '#000000',
+  // YELLOW = '#FFFF00'
+}
+
+let green = COLORS.GREEN
+console.log(green);
+
+enum COLORS {
+  YELLOW = '#FFFF00'
+}
+
+COLORS.YELLOW
