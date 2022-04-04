976b0aee700acac37113a019417eb5cd29a32747
jin1125 <shogo.jinta@gmail.com>
2022-04-04 09:47:54 +0900

    33

---
+++ src/optional.ts
@@ -0,0 +1,13 @@
+export {}
+
+let bmi: (height: number, weight: number, printable?: boolean) => number
+  = (height: number, weight: number, printable?: boolean): number => {
+    const bmi: number = weight / (height * height)
+    if (printable) {
+      console.log(bmi);
+    }
+    return bmi;
+  }
+
+bmi(1.63, 58, true)
+
