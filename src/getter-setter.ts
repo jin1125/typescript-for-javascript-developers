909903f15c65430889f2dfde79548df58de49867
jin1125 <shogo.jinta@gmail.com>
2022-04-05 09:39:39 +0900

    40

---
+++ src/getter-setter.ts
@@ -0,0 +1,34 @@
+export {}
+
+class MyNumberCard {
+  private _owner: string;
+  private _secretNumber: number;
+
+  constructor(owner: string, secretNumber: number) {
+    this._owner = owner;
+    this._secretNumber = secretNumber;
+  }
+
+  get owner() {
+    return this._owner;
+  }
+
+  set secretNumber(secretNumber: number) {
+    this._secretNumber = secretNumber;
+  }
+
+  debugPrint() {
+    return `secretNumber: ${this._secretNumber}`
+  }
+}
+
+let card = new MyNumberCard('jin', 1234567890);
+// card.owner = 'jin';
+console.log(card.owner);
+
+console.log(card.debugPrint());
+card.secretNumber = 1111111111;
+console.log(card.debugPrint());
+
+console.log(card.secretNumber);
+
